800438bc99fda87d7b4573ad2fc6d19abda88893
datCodeGirl <adeolamarsh@gmail.com>
2024-01-21 15:27:18 -0500

    Finished first problem

--- reviewsessions012124.js
+++ reviewsessions012124.js
@@ -6,11 +6,31 @@
 
 
 
-// Given ab array of numbers and a given target. return the target if it's in the array or undefined if it's not
+// Given an array of numbers and a given target. return the target if it's in the array or undefined if it's not
 
+function findTarget(arr, target){
+    for (let i = 0; i < arr.length; i++){
+        let currentNum = arr[i];
+        if (currentNum === target) {
+            return currentNum;
+        } else {
+        }
+        
+    }
+    return undefined;
 
+}
+
+ const arr = [3, 12, 9, 6, 17, 22];
+ console.log(findTarget([3, 12, 9, 6, 17, 22], 12));
+ console.log(findTarget([3, 12, 9, 6, 17, 22], 177));
 
 // Given an array of numbers, return the first number that's bigger than 14 or undefined if no number is bigger than 14
+
+const arr1 = [3, 12, 9, 6, 17,22];
+const findFirstNum = arr1.find((element) => element > 14);
+console.log(findFirstNum([3, 12, 9, 6, 17,22]));
+console.log(findFirstNum([3, 12, 9, 6, 17,22]));
 
 
 
