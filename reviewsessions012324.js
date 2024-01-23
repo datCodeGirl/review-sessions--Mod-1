4bb6592ecad1cc54d5b58f30c1e356f5d374b9a8
datCodeGirl <adeolamarsh@gmail.com>
2024-01-23 18:32:13 -0500

    solved question 1

--- reviewsessions012324.js
+++ reviewsessions012324.js
@@ -1,34 +1,48 @@
 // 1.----------------------------------
 // Write a function that takes in a string and returns a new string with every word capitalized. Write your own examples to call the function with.
 
-// 2.----------------------------------
+
+function capitalizedString(str){
+    const sentence = " The stray dog is dead.";
+        if (str === sentence) {
+        return (sentence.toUpperCase());
+
+        } else {
+         
+         return null;
+
+      }
+}
+
+ console.log(capitalizedString(" The stray dog is dead."));
+// 2.----------------------------------ç=   v       
 // Write a function that takes in an object and returns a formatted greeting.
 // This is the format of the object - 
 
 
-const john = {
-    firstName: "John",
-    lastName: "Klaus",
-    occupation: "Carpenter"
-  }
+// const john = {
+//     firstName: "John",
+//     lastName: "Klaus",
+//     occupation: "Carpenter"
+//   }
   
-  const rebecca = {
-    firstName: "Rebecca",
-    lastName: "Cohen",
-    occupation: "Teacher"
-  }
+//   const rebecca = {
+//     firstName: "Rebecca",
+//     lastName: "Cohen",
+//     occupation: "Teacher"
+//   }
   
-  console.log(formatGreeting(john) === "Hello John Klaus, have a great day at your job as a Carpenter")
+//   console.log(formatGreeting(john) === "Hello John Klaus, have a great day at your job as a Carpenter")
   
-  console.log(formatGreeting(rebecca) === "Hello Rebecca Cohen, have a great day at your job as a Teacher")
+//   console.log(formatGreeting(rebecca) === "Hello Rebecca Cohen, have a great day at your job as a Teacher")
   
-  // 3.----------------------------------
-  // Write a function that takes an array of objects and a targetId and returns a formatted string. Each object in the array will have the same structure as the objects from the previous question plus an id. Use chatGPT to create an array of 10 such objects.
+//   // 3.----------------------------------
+//   // Write a function that takes an array of objects and a targetId and returns a formatted string. Each object in the array will have the same structure as the objects from the previous question plus an id. Use chatGPT to create an array of 10 such objects.
   
-  /* Example Object
-  {
-    id: "Tfjso_4M"
-    firstName: "John",
-    lastName: "Klaus",
-    occupation: "Carpenter"
-  }
+//   /* Example Object
+//   {
+//     id: "Tfjso_4M"
+//     firstName: "John",
+//     lastName: "Klaus",
+//     occupation: "Carpenter"
+//   }
